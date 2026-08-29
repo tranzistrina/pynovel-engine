b6f482dcf08dc93dbc11f3bc605f7a7b6ca6ac52
tranzistrina <tranzistrina@gmail.com>
2026-08-29 17:04:40 +0300

    Export unified save bundle from core API

--- vnengine/core/__init__.py
+++ vnengine/core/__init__.py
@@ -1,4 +1,5 @@
 from .model import Action, Character, ChoiceOption, GameState, Story
 from .rng import DeterministicRNG
+from .save_bundle import SaveBundle
 
-__all__ = ["Action", "Character", "ChoiceOption", "GameState", "Story", "DeterministicRNG"]
+__all__ = ["Action", "Character", "ChoiceOption", "GameState", "Story", "DeterministicRNG", "SaveBundle"]

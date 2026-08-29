4c547aff2d877fd289fe7d1bf4cae9341c8e8e25
tranzistrina <tranzistrina@gmail.com>
2026-08-29 17:52:20 +0300

    Export map command dispatcher

--- vnengine/map/__init__.py
+++ vnengine/map/__init__.py
@@ -15,4 +15,5 @@ from .route_builder import RouteBuilder, RouteRequest
 from .selection import Selectable, SelectionModel
 from .surface import MapMarker, MapSurface
 from .world import MapWorld
-__all__ = ["Camera2D","MapConnection","MapDefinition","MapNode","MapPoint","Route","shortest_path","RouteBuilder","RouteRequest","Movement","MovementController","MovementCommand","MovementOrder","MovementPolicy","TerrainPolicy","TerrainRules","terrain_for","connection_terrain","EntityRegistry","MapEntity","MapWorld","Selectable","SelectionModel","MultiSelection","SelectionChange","MapController","MapEvent","MapAction","MapInteraction","BoxSelector","BoxSelectionController","MapMarker","MapSurface"]
+from .commands import CommandResult, MapCommandDispatcher
+__all__ = ["Camera2D","MapConnection","MapDefinition","MapNode","MapPoint","Route","shortest_path","RouteBuilder","RouteRequest","Movement","MovementController","MovementCommand","MovementOrder","MovementPolicy","TerrainPolicy","TerrainRules","terrain_for","connection_terrain","EntityRegistry","MapEntity","MapWorld","CommandResult","MapCommandDispatcher","Selectable","SelectionModel","MultiSelection","SelectionChange","MapController","MapEvent","MapAction","MapInteraction","BoxSelector","BoxSelectionController","MapMarker","MapSurface"]

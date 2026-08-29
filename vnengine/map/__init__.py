000386ea52bb3e0448ec256cb5435d7174520bfd
tranzistrina <tranzistrina@gmail.com>
2026-08-29 17:12:03 +0300

    Export MapSurface controller API

--- vnengine/map/__init__.py
+++ vnengine/map/__init__.py
@@ -1,10 +1,13 @@
 """Generic data-driven 2D map primitives for strategy and non-linear scenes."""
 
+from .controller import MapController, MapEvent
 from .model import Camera2D, MapConnection, MapDefinition, MapNode, MapPoint
 from .pathfinding import Route, shortest_path
 from .selection import Selectable, SelectionModel
+from .surface import MapMarker, MapSurface
 
 __all__ = [
     "Camera2D", "MapConnection", "MapDefinition", "MapNode", "MapPoint",
     "Route", "shortest_path", "Selectable", "SelectionModel",
+    "MapController", "MapEvent", "MapMarker", "MapSurface",
 ]

da51cd7c0a09a56005518b25f77616ce69616f70
tranzistrina <tranzistrina@gmail.com>
2026-08-29 17:31:46 +0300

    Export route builder and box selection APIs

--- vnengine/map/__init__.py
+++ vnengine/map/__init__.py
@@ -1,16 +1,20 @@
 """Generic data-driven 2D map primitives for strategy and non-linear scenes."""
 
+from .box_controller import BoxSelectionController
+from .box_selection import BoxSelector
 from .controller import MapController, MapEvent
 from .interaction import MapAction, MapInteraction
 from .model import Camera2D, MapConnection, MapDefinition, MapNode, MapPoint
 from .multiselect import MultiSelection, SelectionChange
 from .pathfinding import Route, shortest_path
+from .route_builder import RouteBuilder, RouteRequest
 from .selection import Selectable, SelectionModel
 from .surface import MapMarker, MapSurface
 
 __all__ = [
     "Camera2D", "MapConnection", "MapDefinition", "MapNode", "MapPoint",
-    "Route", "shortest_path", "Selectable", "SelectionModel",
-    "MultiSelection", "SelectionChange", "MapController", "MapEvent",
-    "MapAction", "MapInteraction", "MapMarker", "MapSurface",
+    "Route", "shortest_path", "RouteBuilder", "RouteRequest",
+    "Selectable", "SelectionModel", "MultiSelection", "SelectionChange",
+    "MapController", "MapEvent", "MapAction", "MapInteraction",
+    "BoxSelector", "BoxSelectionController", "MapMarker", "MapSurface",
 ]

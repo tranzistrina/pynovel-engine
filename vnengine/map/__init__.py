cd66eb62cd7988445ed7c2fbe49fb81e0da8060f
tranzistrina <tranzistrina@gmail.com>
2026-08-29 18:05:58 +0300

    Export playable map APIs

--- vnengine/map/__init__.py
+++ vnengine/map/__init__.py
@@ -16,5 +16,7 @@ from .selection import Selectable, SelectionModel
 from .surface import MapMarker, MapSurface
 from .world import MapWorld
 from .world_controller import MapWorldController
+from .playable import MapSelectionHit, PlayableMap
+from .interaction_bridge import PlayableInteractionBridge
 from .commands import CommandResult, MapCommandDispatcher
-__all__ = ["Camera2D","MapConnection","MapDefinition","MapNode","MapPoint","Route","shortest_path","RouteBuilder","RouteRequest","Movement","MovementController","MovementCommand","MovementOrder","MovementPolicy","TerrainPolicy","TerrainRules","terrain_for","connection_terrain","EntityRegistry","MapEntity","MapWorld","MapWorldController","CommandResult","MapCommandDispatcher","Selectable","SelectionModel","MultiSelection","SelectionChange","MapController","MapEvent","MapAction","MapInteraction","BoxSelector","BoxSelectionController","MapMarker","MapSurface"]
+__all__ = ["Camera2D","MapConnection","MapDefinition","MapNode","MapPoint","Route","shortest_path","RouteBuilder","RouteRequest","Movement","MovementController","MovementCommand","MovementOrder","MovementPolicy","TerrainPolicy","TerrainRules","terrain_for","connection_terrain","EntityRegistry","MapEntity","MapWorld","MapWorldController","MapSelectionHit","PlayableMap","PlayableInteractionBridge","CommandResult","MapCommandDispatcher","Selectable","SelectionModel","MultiSelection","SelectionChange","MapController","MapEvent","MapAction","MapInteraction","BoxSelector","BoxSelectionController","MapMarker","MapSurface"]

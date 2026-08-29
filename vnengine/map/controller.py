7207d2ae933aa7960b589ca90f3c2b049b2c05af
tranzistrina <tranzistrina@gmail.com>
2026-08-29 17:25:34 +0300

    Integrate MapController with EventBus-compatible emitter

--- vnengine/map/controller.py
+++ vnengine/map/controller.py
@@ -12,20 +12,26 @@ class MapEvent:
 
 
 class MapController:
-    """Input-to-event bridge for MapSurface; game rules stay external."""
+    """Input-to-event bridge. The callback may be an EventBus emitter."""
 
     def __init__(self, surface: MapSurface, emit=None):
         self.surface = surface
         self.emit = emit or (lambda event: None)
         self._dragging = False
 
+    def _publish(self, event: MapEvent) -> None:
+        try:
+            self.emit(event.name, {"target_id": event.target_id, **event.data})
+        except TypeError:
+            self.emit(event)
+
     def select(self, pos: tuple[int, int]) -> Any:
         hit = self.surface.select_at(pos)
         if hit is None:
-            self.emit(MapEvent("map.selection_cleared", None, {}))
+            self._publish(MapEvent("map.selection_cleared", None, {}))
             return None
         event_name = "map.marker_selected" if hit.__class__.__name__ == "MapMarker" else "map.node_selected"
-        self.emit(MapEvent(event_name, hit.id, {"object": hit}))
+        self._publish(MapEvent(event_name, hit.id, {"object": hit}))
         return hit
 
     def begin_pan(self, pos: tuple[int, int]) -> None:
@@ -35,7 +41,7 @@ class MapController:
     def move_pan(self, pos: tuple[int, int]) -> None:
         if self._dragging:
             self.surface.pan_to(pos)
-            self.emit(MapEvent("map.camera_changed", None, {"x": self.surface.camera.x, "y": self.surface.camera.y, "zoom": self.surface.camera.zoom}))
+            self._publish(MapEvent("map.camera_changed", None, {"x": self.surface.camera.x, "y": self.surface.camera.y, "zoom": self.surface.camera.zoom}))
 
     def end_pan(self) -> None:
         self._dragging = False
@@ -43,8 +49,8 @@ class MapController:
 
     def zoom(self, factor: float, pos: tuple[int, int]) -> None:
         self.surface.zoom_at(factor, pos)
-        self.emit(MapEvent("map.camera_changed", None, {"x": self.surface.camera.x, "y": self.surface.camera.y, "zoom": self.surface.camera.zoom}))
+        self._publish(MapEvent("map.camera_changed", None, {"x": self.surface.camera.x, "y": self.surface.camera.y, "zoom": self.surface.camera.zoom}))
 
     def set_route(self, node_ids: list[str]) -> None:
         self.surface.set_route(node_ids)
-        self.emit(MapEvent("map.route_changed", None, {"nodes": list(node_ids)}))
+        self._publish(MapEvent("map.route_changed", None, {"nodes": list(node_ids)}))

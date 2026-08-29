ff3ffd63e171498a06640e04821a6e6641ee5424
tranzistrina <tranzistrina@gmail.com>
2026-08-29 17:47:18 +0300

    Export terrain APIs

--- vnengine/map/__init__.py
+++ vnengine/map/__init__.py
@@ -1,5 +1,4 @@
 """Generic data-driven 2D map primitives for strategy and non-linear scenes."""
-
 from .box_controller import BoxSelectionController
 from .box_selection import BoxSelector
 from .controller import MapController, MapEvent
@@ -7,17 +6,11 @@ from .interaction import MapAction, MapInteraction
 from .model import Camera2D, MapConnection, MapDefinition, MapNode, MapPoint
 from .movement import Movement, MovementController
 from .movement_command import MovementCommand, MovementOrder
+from .movement_policy import MovementPolicy, TerrainPolicy
+from .terrain import TerrainRules, connection_terrain, terrain_for
 from .multiselect import MultiSelection, SelectionChange
 from .pathfinding import Route, shortest_path
 from .route_builder import RouteBuilder, RouteRequest
 from .selection import Selectable, SelectionModel
 from .surface import MapMarker, MapSurface
-
-__all__ = [
-    "Camera2D", "MapConnection", "MapDefinition", "MapNode", "MapPoint",
-    "Route", "shortest_path", "RouteBuilder", "RouteRequest",
-    "Movement", "MovementController", "MovementCommand", "MovementOrder",
-    "Selectable", "SelectionModel", "MultiSelection", "SelectionChange",
-    "MapController", "MapEvent", "MapAction", "MapInteraction",
-    "BoxSelector", "BoxSelectionController", "MapMarker", "MapSurface",
-]
+__all__ = ["Camera2D","MapConnection","MapDefinition","MapNode","MapPoint","Route","shortest_path","RouteBuilder","RouteRequest","Movement","MovementController","MovementCommand","MovementOrder","MovementPolicy","TerrainPolicy","TerrainRules","terrain_for","connection_terrain","Selectable","SelectionModel","MultiSelection","SelectionChange","MapController","MapEvent","MapAction","MapInteraction","BoxSelector","BoxSelectionController","MapMarker","MapSurface"]

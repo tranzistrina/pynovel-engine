fb2656373deb2f9c93ae586e5d9879a9092b1f34
tranzistrina <tranzistrina@gmail.com>
2026-08-29 13:46:53 +0300

    Test branching language

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,7 @@
 from vnengine.script.parser import VNParser
 
-def test_parse():
-    story = VNParser().parse('''label start\nsay Alice "Hello world"\nset affection = 5\nchoice\n  "Stay": start\n''')
-    assert story.labels['start'] == 0
-    assert [a.kind for a in story.actions] == ['say','set','choice']
-    assert story.actions[-1].data['options'][0].target == 'start'
+def test_parser_choices_conditions():
+    story=VNParser().parse('''label start\nset affection = 2\nif affection >= 2\nsay Alice "Hello"\nelse\nsay Alice "Bye"\nendif\nchoice\n"Continue": start\n"End": ending\nlabel ending\nend\n''')
+    assert story.labels['start']==0
+    assert story.labels['ending']>0
+    assert [a.kind for a in story.actions]==['set','if','say','else','say','endif','choice','end']

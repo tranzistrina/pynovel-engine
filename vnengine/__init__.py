8e090c0d25e1d37e933a0c7f1fef797936e1ff00
tranzistrina <tranzistrina@gmail.com>
2026-08-30 22:14:06 +0300

    Expose unified runtime contract

--- vnengine/__init__.py
+++ vnengine/__init__.py
@@ -1,1 +1,11 @@
 __version__ = "0.40.0"
+
+from .runtime_protocol import CoreRuntimeAdapter, ExtensibleRuntimeAdapter, RuntimeProtocol, require_runtime
+
+__all__ = [
+    "__version__",
+    "RuntimeProtocol",
+    "require_runtime",
+    "CoreRuntimeAdapter",
+    "ExtensibleRuntimeAdapter",
+]
